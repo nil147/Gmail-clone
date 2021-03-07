99e04dcb3795fcbcac5c9b4af41334f2d166b501
nil147 <68010556+nil147@users.noreply.github.com>
2021-03-07 14:57:25 +0200

    first upload

---
+++ gmail/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+    return (
+        <HeaderContainer>
+
+        </HeaderContainer>
+    )
+}
+
+export default Header
+
+const HeaderContainer = styled.div``;
